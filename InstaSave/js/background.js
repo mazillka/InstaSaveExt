1a226fa33d56cde85ebb87bba1bf0654e7da20e6
Mazillka <mazillko@gmail.com>
2015-02-28 13:00:50 +0200

    5
    some dynamic context menu improvements and code cleanup

---
+++ InstaSave/js/background.js
@@ -0,0 +1,50 @@
+var Url;
+chrome.extension.onMessage.addListener(function (request) {
+	Url = request.Link;
+
+	switch (request.Type) {
+	case 'create':
+		chrome.contextMenus.create({
+			id : 'subMenu',
+			enabled : false,
+			title : 'Save as...',
+			contexts : ['all'],
+			documentUrlPatterns : ['*://instagram.com/*']
+		});
+		break;
+	case 'image':
+		chrome.contextMenus.update('subMenu', {
+			title : 'Save image as...',
+			enabled : true
+		});
+		break;
+	case 'video':
+		chrome.contextMenus.update('subMenu', {
+			title : 'Save video as...',
+			enabled : true
+		});
+		break;
+	case 'none':
+		chrome.contextMenus.update('subMenu', {
+			enabled : false,
+			title : 'nothin to save'
+		});
+		break;
+	}
+});
+
+function Save() {
+	var a = document.createElement('a');
+	a.href = Url;
+	a.download = 'temp.*';
+	a.click();
+	a.remove();
+}
+
+chrome.contextMenus.onClicked.addListener(Save);
+
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.tabs.create({
+		'url' : chrome.extension.getURL('html/donate.html')
+	});
+});
---
+++ InstaSave/js/contentscript.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+	chrome.extension.sendMessage({
+		Type : 'create',
+		Link : 'none'
+	});
+}
+
+document.addEventListener('mousedown', function (event) {
+	if (event.button !== 2) {
+		return false;
+	}
+
+	if (event.button === 2) {
+		var Url, imgUrl = event.srcElement.style.backgroundImage.replace('url(', '').replace(')', ''), videoUrl = event.srcElement.parentElement.getAttribute('src');
+
+		if (videoUrl === null) {
+			Url = imgUrl;
+		} else {
+			Url = videoUrl;
+		}
+
+		if (Url.indexOf('n.jpg') > 0) {
+			chrome.extension.sendMessage({
+				Type : 'image',
+				Link : Url
+			});
+		} else if (Url.indexOf('n.mp4') > 0) {
+			chrome.extension.sendMessage({
+				Type : 'video',
+				Link : Url
+			});
+		} else {
+			chrome.extension.sendMessage({
+				Type : 'none',
+				Link : 'none'
+			});
+		}
+	}
+});
