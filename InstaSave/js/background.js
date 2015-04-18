0caf3f79525c5270007acc1b3ee36550a5b2eb93
Mazillka <mazillko@gmail.com>
2015-04-18 11:59:37 +0300

    9
    some code clean up
    added keyboard shortcut

--- InstaSave/js/background.js
+++ InstaSave/js/background.js
@@ -1,34 +1,34 @@
 var Url;
+
+function UpdateContextMenu(id, titleValue, status) {
+	chrome.contextMenus.update(id, {
+		title : titleValue,
+		enabled : status
+	});
+}
+
 chrome.extension.onMessage.addListener(function (request) {
 	Url = request.Link;
 
 	switch (request.Type) {
 	case 'create':
+		chrome.contextMenus.removeAll();
 		chrome.contextMenus.create({
 			id : 'menu',
 			enabled : false,
 			title : 'Save as...',
 			contexts : ['all'],
-			documentUrlPatterns : ['*://instagram.com/*']			
+			documentUrlPatterns : ['*://instagram.com/*']
 		});
 		break;
 	case 'image':
-		chrome.contextMenus.update('menu', {
-			title : 'Save image as...',
-			enabled : true
-		});
+		UpdateContextMenu('menu', 'Save image as...', true);
 		break;
 	case 'video':
-		chrome.contextMenus.update('menu', {
-			title : 'Save video as...',
-			enabled : true
-		});
+		UpdateContextMenu('menu', 'Save video as...', true);
 		break;
 	case 'none':
-		chrome.contextMenus.update('menu', {
-			title : 'nothin to save',
-			enabled : false
-		});
+		UpdateContextMenu('menu', 'nothin to save', false);
 		break;
 	}
 });
@@ -39,14 +39,48 @@ chrome.contextMenus.onClicked.addListener(function (info) {
 	});
 });
 
+chrome.commands.onCommand.addListener(function (command) {
+	switch (command) {
+	case "Save as...":
+		chrome.tabs.query({
+			active : true
+		}, function (tab) {
+			chrome.tabs.sendMessage(tab[0].id, {
+				method : 'getUrl'
+			}, function (response) {
+				try {
+					if (response.url != 'none') {
+						chrome.downloads.download({
+							url : response.url
+						});
+					} else {
+						chrome.notifications.clear('msg');
+						chrome.notifications.create('msg', {
+							type : "basic",
+							title : "InstaSave",
+							message : "Nothin to save, place cursor on image or video and try again",
+							iconUrl : "../icons/128x128.png"
+						}, function () {
+							setTimeout(function () {
+								chrome.notifications.clear('msg');
+							}, 5000);
+						});
+					}
+				} catch (e) {}
+			});
+		});
+		break;
+	}
+});
+
 chrome.runtime.onInstalled.addListener(function (details) {
 	if (details.reason == 'install') {
 		chrome.tabs.create({
-			url : chrome.extension.getURL('html/donate.html')
+			url : 'http://mazillka.in.ua/donate/'
 		});
 	} else if (details.reason == 'update') {
 		chrome.tabs.create({
-			url : chrome.extension.getURL('html/donate.html')
-		});			
+			url : 'http://mazillka.in.ua/donate/'
+		});
 	}
 });
--- InstaSave/js/contentscript.js
+++ InstaSave/js/contentscript.js
@@ -5,32 +5,61 @@ window.onload = function () {
 	});
 }
 
+function SendMessage(type, link) {
+	chrome.extension.sendMessage({
+		Type : type,
+		Link : link
+	});
+}
+
+function GetUrl(element) {
+	var Url;
+	var imgUrl = element.style.backgroundImage.replace('url(', '').replace(')', '');
+	var videoUrl = element.parentElement.getAttribute('src');
+
+	if (videoUrl === null) {
+		Url = imgUrl;
+	} else {
+		Url = videoUrl;
+	}
+
+	return Url;
+}
+
 document.addEventListener('mousedown', function (event) {
 	if (event.button === 2) {
-		var Url;
-		var imgUrl = event.srcElement.style.backgroundImage.replace('url(', '').replace(')', '');
-		var videoUrl = event.srcElement.parentElement.getAttribute('src');
+		var url = GetUrl(event.srcElement);
 
-		if (videoUrl === null) {
-			Url = imgUrl;
+		if (url.indexOf('n.jpg') > 0) {
+			SendMessage('image', url);
+		} else if (url.indexOf('n.mp4') > 0) {
+			SendMessage('video', url);
 		} else {
-			Url = videoUrl;
+			SendMessage('none', 'none');
 		}
+	}
+});
 
-		if (Url.indexOf('n.jpg') > 0) {
-			chrome.extension.sendMessage({
-				Type : 'image',
-				Link : Url
-			});
-		} else if (Url.indexOf('n.mp4') > 0) {
-			chrome.extension.sendMessage({
-				Type : 'video',
-				Link : Url
+function GetElementUnderCursor() {
+	var x = document.querySelector(":hover");
+	while (x) {
+		var xxx = x;
+		x = xxx.querySelector(":hover");
+	}
+	return xxx;
+}
+
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.method == 'getUrl') {
+		var url = GetUrl(GetElementUnderCursor());
+
+		if (url.indexOf('n.jpg') > 0 || url.indexOf('n.mp4') > 0) {
+			sendResponse({
+				url : url
 			});
 		} else {
-			chrome.extension.sendMessage({
-				Type : 'none',
-				Link : 'none'
+			sendResponse({
+				url : 'none'
 			});
 		}
 	}
