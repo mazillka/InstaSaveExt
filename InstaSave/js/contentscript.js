1bff15b281608abda42a2617dbc3a4b6ca4821bb
Andrew Shevchuk-Yuganets <mazillko@gmail.com>
2015-07-13 22:14:17 +0300

    15

--- InstaSave/js/contentscript.js
+++ InstaSave/js/contentscript.js
@@ -1,67 +1,77 @@
-// window.onload = function () {
-	// chrome.extension.sendMessage({
-		// Type : "create",
-		// Link : "none"
-	// });
-// }
-
 function SendMessage(type, link) {
 	chrome.extension.sendMessage({
 		Type : type,
 		Link : link
 	});
 }
-
-// TODO: 
+ 
 function GetUrl(element) { 
 	var Url;
-	var imgUrl = element.style.backgroundImage.replace("url(", "").replace(")", "");
-	var videoUrl = element.parentElement.getAttribute("src");
-
-	if (videoUrl === null) {
-		Url = imgUrl;
-	} else {
-		Url = videoUrl;
+	try{
+		var imgUrl = element.parentElement.getAttribute("src");
+		var videoUrl = element.previousSibling.firstChild.getAttribute("src");
+	} catch(e) {}
+	
+	// console.log("Image: " + imgUrl);
+	// console.log("Video: " + videoUrl);
+	
+	if (videoUrl === undefined){
+		return imgUrl;
 	}
-
-	return Url;
+	
+	if(imgUrl === null){
+		return videoUrl;
+	}
+	
+	return null;
 }
 
 document.addEventListener("mousedown", function (event) {
 	if (event.button === 2) {
 		var url = GetUrl(event.srcElement);
 
+		//console.log("Url: " + url);
+		
+		if(url === null){
+			SendMessage("none", "none");
+			return;
+		}
+		
 		if (url.indexOf("n.jpg") > 0) {
 			SendMessage("image", url);
-		} else if (url.indexOf("n.mp4") > 0) {
+			return;
+		} 
+		if (url.indexOf("n.mp4") > 0) {
 			SendMessage("video", url);
-		} else {
-			SendMessage("none", "none");
+			return;
 		}
 	}
 });
 
-// function GetElementUnderCursor() {
-	// var x = document.querySelector(":hover");
-	// while (x) {
-		// var xxx = x;
-		// x = xxx.querySelector(":hover");
-	// }
-	// return xxx;
-// }
+function GetElementUnderCursor() {
+	var x = document.querySelector(":hover");
+	while (x) {
+		var element = x;
+		x = element.querySelector(":hover");
+	}
+	return element;
+}
 
-// chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-	// if (request.method == "getUrl") {
-		// var url = GetUrl(GetElementUnderCursor());
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+	if (request.method == "getUrl") {
+		var url = GetUrl(GetElementUnderCursor());
 
-		// if (url.indexOf("n.jpg") > 0 || url.indexOf("n.mp4") > 0) {
-			// sendResponse({
-				// url : url
-			// });
-		// } else {
-			// sendResponse({
-				// url : "none"
-			// });
-		// }
-	// }
-// });
+		if(url === null){
+			sendResponse({
+				url : "none"
+			});
+			return;
+		}
+		
+		if (url.indexOf("n.jpg") > 0 || url.indexOf("n.mp4") > 0) {
+			sendResponse({
+				url : url
+			});
+		}
+	}
+});
