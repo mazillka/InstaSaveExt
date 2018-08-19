8f49318d05b18f959c0d2f4776c405d13fcab8c0
Andrii Shevchuk-Yuhanets <mazillko@gmail.com>
2018-08-19 19:59:03 +0300

    ...

--- InstaSave/js/contentscript.js
+++ InstaSave/js/contentscript.js
@@ -1,3 +1,5 @@
+var debug = false;
+
 function SendMessage(type, link) {
     chrome.extension.sendMessage({
         Type: type,
@@ -8,20 +10,28 @@ function SendMessage(type, link) {
 document.addEventListener("mousedown", function(event) {
     if (event.button === 2) {
         try {
-            var video = event.srcElement.previousSibling.previousSibling.firstChild.firstChild.src;
-            // console.log("video: " + video);
+            var video = event.srcElement.previousSibling.previousSibling.firstChild.firstChild.firstChild.src;
+            if(debug){
+                console.log("video: " + video);
+            }
             SendMessage("video", video);
         } catch (e) {
             try {
                 var image = event.srcElement.previousSibling.firstChild.src;
                 if (image === undefined || image == 'none') {
-                    // console.log("image: " + event.srcElement.previousSibling.firstChild.src);
+                    if(debug){
+                        console.log("image: " + event.srcElement.previousSibling.firstChild.src);
+                    }
                     throw (e);
                 }
-                // console.log("image: " + image);
+                if(debug){
+                    console.log("image: " + image);
+                }
                 SendMessage("image", image);
             } catch (e) {
-                // console.log("none");
+                if(debug){
+                    console.log("none");
+                }
                 SendMessage("none", "none");
             }
         }
